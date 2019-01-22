c7edf99d2f0345c72260a10afdae2770376e6adc
jiansoco27 <ian.soco.27@gmail.com>
2019-01-22 17:41:17 +0800

    initial commit

---
+++ src/testJava/HelloWorld.java
@@ -0,0 +1,12 @@
+package testJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World - This is Selenium Training");
+		System.out.println("Hellow World 2");
+	}
+
+}
